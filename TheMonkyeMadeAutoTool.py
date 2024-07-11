5cbc42b4b813740db561d5a1b2ddebf19ecb80e9
TDOGROCS <133181000+TDOGROCS@users.noreply.github.com>
2024-07-10 21:07:42 -0400

    Update TheMonkyeMadeAutoTool.py

--- TheMonkyeMadeAutoTool.py
+++ TheMonkyeMadeAutoTool.py
@@ -152,6 +152,8 @@ elif user_selection == 3:
     username = input()
     password_list = [12345, Password, bananas]
     ssh_brute_force(target, username, password_list)
+
+
 elif user_selection == 4:
     print(f"{bcolors.OKGREEN}Enter the port you want to scan...")
     user_port_num = input()
@@ -160,8 +162,6 @@ elif user_selection == 4:
         nmapscan = subprocess.run(f'nmap -sV -sT -Pn -v {user_port_num}',capture_output=True, shell=True, text=True)
         print("Working on it, have some patience!")
         print(nmapscan.stdout)
-        
-        
         
     except Exception as e:
         print(f'Failed you are dog shit! {e}')
